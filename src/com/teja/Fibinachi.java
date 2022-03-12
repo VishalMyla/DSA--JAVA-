d67c33d55a76fb43092d3db6948f237985db592a
vishalmyla <vishalmyla@gmail.com>
2022-03-12 15:53:28 +0530

    Binary_recurion you can check that out

---
+++ src/com/teja/Fibinachi.java
@@ -0,0 +1,20 @@
+package com.teja;
+
+public class Fibinachi {
+//Only small numbers
+
+    public static void main(String[] args) {
+        System.out.println(finbonachii(4));
+
+    }
+    static int finbonachii(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+
+        else{
+            return finbonachii(n-1)+finbonachii(n-2);
+        }
+    }
+
+}
