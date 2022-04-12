915812dfb698b13516f11345d0106934db5d7067
vishalmyla <vishalmyla@gmail.com>
2022-04-12 20:14:24 +0530

    Binary algorithm is added

---
+++ src/com/Binary_search_Problem/binary_alg.java
@@ -0,0 +1,28 @@
+package com.Binary_search_Problem;
+
+public class binary_alg {
+    public static void main(String[] args) {
+        int[] arr={1};
+        int target=1;
+        System.out.println(binary(arr,target));
+    }
+//    this will return the index value of the target element
+//    return -1 if the target doesn't exist.
+    static int binary(int[] arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
