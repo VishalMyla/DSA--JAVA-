1859e4955bf853bb44d03229cfe0655b334a55c2
vishalmyla <vishalmyla@gmail.com>
2022-04-14 19:45:24 +0530

    A new Problem is added in binary search problems

---
+++ src/com/Binary_search_Problem/Sqrt_x.java
@@ -0,0 +1,23 @@
+package com.Binary_search_Problem;
+
+class Sqrt_x {
+    public int mySqrt(int x) {
+        int start=1,end=x;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(mid==x/mid){
+                return mid;
+            }
+            else if(mid>x/mid){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+            
+        }
+        
+        return end;
+    }
+}
