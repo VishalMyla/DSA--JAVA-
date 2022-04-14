4085602c20785d91ea011badc1f34ec570ab186a
vishalmyla <vishalmyla@gmail.com>
2022-04-14 19:26:55 +0530

    A new Problem is added in binary search problems

---
+++ src/com/Binary_search_Problem/Search_Insert_Position.java
@@ -0,0 +1,21 @@
+package com.Binary_search_Problem;
+
+class Search_Insert_Position {
+    public int searchInsert(int[] arr, int target) {
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+                
+        }
+        return start;
+    }
+}
