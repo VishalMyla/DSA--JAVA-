9f05403b1ac881db27c23a82c98d3d75e02aea21
vishalmyla <vishalmyla@gmail.com>
2022-05-06 09:56:40 +0530

    A new Problem is added in binary search problems

---
+++ src/com/Matrix_Questions/Row_coloumn_Matrix.java
@@ -0,0 +1,33 @@
+package com.Matrix_Questions;
+
+import java.util.Arrays;
+
+public class Row_coloumn_Matrix {
+    public static void main(String[] args) {
+        int[][] matrix={
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}
+        };
+        int target=8;
+        int[] arr=search(matrix,target);
+        System.out.println(Arrays.toString(arr));
+    }
+    static int[] search(int[][] matrix,int target){
+      int row=0,col= matrix[0].length-1;
+      while(row< matrix.length && col>=0){
+          if(matrix[row][col]==target){
+              return new int[]{row,col};
+          }
+          else if (matrix[row][col]>target){
+              col--;
+          }
+          else{
+              row++;
+          }
+      }
+      return new int[]{-1,-1};
+
+
+    }
+}
