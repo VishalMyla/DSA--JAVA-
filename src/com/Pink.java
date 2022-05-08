df3a7e5b278da0d4edf6d5579ab73edc06ca2459
VishalMyla <99010982+VishalMyla@users.noreply.github.com>
2022-05-08 11:36:30 +0530

    Add files via upload

---
+++ src/com/Pink.java
@@ -0,0 +1,10 @@
+package ocm.Imp;
+import java.util.Scanner;
+public class Pink {
+    public static void main(String args[]){
+        System.out.println("Enter the Name:");
+        Scanner sc= new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
