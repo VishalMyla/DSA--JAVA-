4430d8991d718c676285cfab33548b271f6c2652
vishalmyla <vishalmyla@gmail.com>
2022-05-12 14:00:03 +0530

    A new Problem is added in binary search problems

---
+++ src/com/Array_Questions/Move_Zeros_to_End.java
@@ -0,0 +1,30 @@
+package com.Array_Questions;
+
+import java.util.Arrays;
+
+// brute force
+public class Move_Zeros_to_End {
+    public static void main(String[] args) {
+        int[] arr={5};
+        method(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void method(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                for(int j=i+1;j< arr.length;j++) {
+                    if(arr[j]!=0){
+                        swap(arr,i,j);
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+    static void swap(int[] arr,int a,int b){
+        int test=arr[a];
+        arr[a]=arr[b];
+        arr[b]=test;
+    }
+}
